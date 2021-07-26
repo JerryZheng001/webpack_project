67733a42ff58e1764206bb43dc5312ac57cb8112
Qu.jiajun <qujiajun@8lab.cn>
2021-07-26 16:09:50 +0800

    "@babel/preset-react"配置

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,7 @@ module.exports = {
                                     useBuiltIns: 'usage',
                                 },
                             ],
+                            "@babel/preset-react"
 
                         ],
                         plugins: [
